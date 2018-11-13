0527395ceaa7fff4dce2961f8a99de937abbe636
xw5 <412832768@qq.com>
2018-11-13 22:21:27 +0800

    开发评论和周未去哪儿

--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -3,6 +3,8 @@
     <home-header></home-header>
     <home-swiper></home-swiper>
     <home-icons></home-icons>
+    <home-recommend></home-recommend>
+    <home-weekend></home-weekend>
   </div>
 </template>
 
@@ -10,6 +12,8 @@
 import HomeHeader from './components/Header.vue'
 import HomeSwiper from './components/Swiper.vue'
 import HomeIcons from './components/Icons.vue'
+import HomeRecommend from './components/Recommend.vue'
+import HomeWeekend from './components/Weekend.vue'
 export default {
   name: 'home',
   data () {
@@ -20,7 +24,9 @@ export default {
   components: {
     HomeHeader,
     HomeSwiper,
-    HomeIcons
+    HomeIcons,
+    HomeRecommend,
+    HomeWeekend
   }
 }
 </script>
