ff3a854f77709464829dbb88aa5bfc3b9c309e3f
xw5 <412832768@qq.com>
2018-12-03 23:19:35 +0800

    '增加keep-alive'

--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -15,6 +15,7 @@ import HomeIcons from './components/Icons.vue'
 import HomeRecommend from './components/Recommend.vue'
 import HomeWeekend from './components/Weekend.vue'
 import axios from 'axios'
+import {mapState} from 'vuex'
 export default {
   name: 'home',
   data () {
@@ -23,7 +24,8 @@ export default {
       swiperList: [],
       iconList: [],
       recommendList: [],
-      weekendList: []
+      weekendList: [],
+      lastCity: ''
     }
   },
   components: {
@@ -34,12 +36,22 @@ export default {
     HomeWeekend
   },
   mounted () {
+    this.lastCity = this.city
     this.getHomeInfo()
+  },
+  activated () {
+    if(this.lastCity != this.city){
+        this.lastCity = this.city
+        this.getHomeInfo()
+      }
+  },
+  computed: {
+    ...mapState(['city'])
   },
   methods: {
     getHomeInfo () {
-      axios.get('/api/index.json')
-        .then(this.getHomeInforSuccess)
+      axios.get('/api/index.json?city=' + this.city)
+      .then(this.getHomeInforSuccess)
     },
     getHomeInforSuccess (res) {
       var resData = res.data
